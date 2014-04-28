22f6618667002e19d1c33feaa16b5f195030f249
Luiz Fernando Oliveira Corte Real <sr.saude@gmail.com>
2014-04-27 22:03:04 -0300

    Adicionando link para o icone do calendario

--- wp-content/themes/event-manager/functions.php
+++ wp-content/themes/event-manager/functions.php
@@ -208,9 +208,11 @@ function sc_custom_header_style() {
 function cs_event_description() {
 
 	$date = '<div class="one-third first"><div class="date">
+	<a href="programa">
 	<span class="icon">' . genesis_get_option( 'event_date_icon', 'social-coup' ) . '</span>
 	<span class="title">' . pll__('Date Title') . '</span>
 	<span class="subtitle">' . pll__('Date Subtitle') . '</span>
+	</a>
 	</div></div>';
 	
 	$location = '<div class="one-third"><div class="location">
